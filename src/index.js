75f4ee1749acf6d5d5d1dee4799aae6f0f79552c
Neeharika Srini <neeharika.srinivasan@gmail.com>
2024-03-30 18:22:57 +0530

    updated sass files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import '../src/sass/main.css';
+import '../src/sass/main.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,4 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
